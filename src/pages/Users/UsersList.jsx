e4ef47c81d43a9bde5b9be7f68287f168ce096c3
Dev <devgoel2004@gmail.com>
2023-11-26 18:26:39 +0530

    Errors resolved

--- src/pages/Users/UsersList.jsx
+++ src/pages/Users/UsersList.jsx
@@ -3,12 +3,10 @@ import { useSelector } from "react-redux";
 import User from "./User";
 const UsersList = () => {
   const users = useSelector((state) => state.usersReducer);
-  const Users = users.allUsersDetails;
-
   return (
     <div className="userList-container">
-      {Users ? (
-        Users.map((user) => <User user={user} key={user?._id} />)
+      {users ? (
+        users.map((user) => <User user={user} key={user?._id} />)
       ) : (
         <h1>Loading..</h1>
       )}
